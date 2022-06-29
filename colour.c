e8c0222ebd5a541427253a76ae386bb5be5d0992
Anahit K <anikakasamanyan@gmail.com>
2022-06-29 18:08:21 +0200

    ok, now the colours should be fine

--- colour.c
+++ colour.c
@@ -6,17 +6,20 @@
 /*   By: ankasamanyan <ankasamanyan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 00:24:40 by ankasamanya       #+#    #+#             */
-/*   Updated: 2022/06/28 19:27:38 by ankasamanya      ###   ########.fr       */
+/*   Updated: 2022/06/29 18:00:59 by ankasamanya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-// int	get_colour(t_colour colour)
-// {
-// 	printf("%d %d %d %d\n", colour.t, colour.r, colour.g, colour.b);
-// 	return (create_trgb(get_t(colour.t), get_r(colour.r), get_g(colour.g), get_b(colour.b)));
-// }
+int	get_colour(t_colour colour)
+{
+	int	trgb;
+	// printf("%d %d %d %d\n", colour.t, colour.r, colour.g, colour.b);
+	// return (create_trgb(get_t(colour.t), get_r(colour.r), get_g(colour.g), get_b(colour.b)));
+	trgb = colour.t << 24 | colour.r << 16 | colour.g << 8 | colour.b;
+	return (trgb);
+}
 
 int	og_colours(t_colour colour, int iter)
 {
@@ -28,7 +31,8 @@ int	og_colours(t_colour colour, int iter)
 	colour.r = 9.0 * (1.0 - t) * pow(t, 3.0) * 255;
 	colour.g = 15.0 * pow((1.0 - t), 2.0) * pow(t, 2.0) * 255; 
 	colour.b = 8.5 * pow((1.0 - t), 3.0) * t * 255;
-	trgb = colour.t << 24 | colour.r << 16 | colour.g << 8 | colour.b;
+	
+	// trgb = colour.t << 24 | colour.r << 16 | colour.g << 8 | colour.b;
 	// trgb = get_colour(colour);
-	return (trgb);
+	return (get_colour(colour));
 }
--- trgb.c
+++ trgb.c
@@ -6,35 +6,8 @@
 /*   By: ankasamanyan <ankasamanyan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 00:14:55 by ankasamanya       #+#    #+#             */
-/*   Updated: 2022/06/28 19:27:19 by ankasamanya      ###   ########.fr       */
+/*   Updated: 2022/06/29 17:59:50 by ankasamanya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-// int	create_trgb(unsigned char t, unsigned char r, unsigned char g, unsigned char b)
-// {
-// 	//return (*(int *)(unsigned char [4]){r, g, b, t});
-// 	//printf("%d %d %d %d\n", t,r,g,b);
-// 	return (((int)t) << 24 | ((int)r) << 16 | ((int)g) << 8 | ((int)b));
-// }
-
-// unsigned char	get_t(int trgb)
-// {
-// 	return (((unsigned char *)&trgb)[3]);
-// }
-
-// unsigned char	get_r(int trgb)
-// {
-// 	return (((unsigned char *)&trgb)[2]);
-// }
-
-// unsigned char	get_g(int trgb)
-// {
-// 	return (((unsigned char *)&trgb)[1]);
-// }
-
-// unsigned char	get_b(int trgb)
-// {
-// 	return (((unsigned char *)&trgb)[0]);
-// }
